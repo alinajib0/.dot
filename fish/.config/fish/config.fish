be7ccfcbd426226bb5ece7485f33a4955f819571
ali najib <alinajibhegazy@proton.me>
2025-07-14 18:48:22 +0300

    nnn config in fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -18,6 +18,7 @@ set -gx SHELL fish
 set -gx TERMINAL alacritty
 set -gx BROWSER librewolf
 set -gx FILE_BROWSER nnn
+set -gx PAGER less
 
 # set -gx PATH $PATH ~/.config/tmux/plugins/tmuxifier/bin
 
@@ -73,7 +74,8 @@ fi=37"
 # dracula fzf
 set -Ux FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
 
-export NNN_PLUG='n:nuke;j:autojump;o:fzopen;p:preview-tui;t:preview-tabbed;i:imgview;f:fzcd;d:dragdrop;m:nmount;x:xdgdefault;'
+# set -gx NNN_PLUG='n:nuke;j:autojump;o:fzopen;p:preview-tui;t:preview-tabbed;i:imgview;f:fzcd;m:nmount;x:xdgdefault;p:finder;'
+set -gx NNN_PLUG 'j:autojump;f:fzopen;l:fzplug;c:gitroot;s:!git status -s;g:!lazygit;m:nmount;S:suedit;k:pskill;x:xdgdefault;t:preview-tui;p:preview-tabbed'
 
 # set -gx NNN_OPENER "/home/alinajib/.config/nnn/plugins/nuke" # Default opener for GUI files
 set -gx NNN_COLORS 5236
@@ -82,6 +84,10 @@ set -gx NNN_COLORS 5236
 set -gx NNN_FCOLORS 0404040000000600010F0F02
 set -gx NNN_FIFO "/tmp/nnn.fifo"
 set -gx NNN_FCOLORS 0404040000000600010F0F02
-set -gx NNN_BMS "h:~;d:~/Download;p:~/photo;"
+# set -gx NNN_BMS "h:~;d:~/Download;p:~/photo;c:~/.config;"
+set -gx NNN_TRASH 1
+set -gx NNN_MCLICK ls # for midle click
+
+alias lsn "cat ~/.config/nnn/.selection | tr '\\0' '\\n'"
 
 
